74f5e3163d89cd0ee6968204defeea0a15e6f79e
Berk Dehrioğlu <jarvis@Berk-MacBook-Pro.local>
2017-11-03 11:00:49 +0300

    Database Connection String

--- server.py
+++ server.py
@@ -1,11 +1,34 @@
 import datetime
 import os
+import psycopg2
+import sys
+
+
 
 from flask import Flask
 from flask import render_template
 
 
 app = Flask(__name__)
+
+
+dsn_database = "compose>"       
+dsn_hostname = "sl-us-south-1-portal.6.dblayer.com"     
+dsn_port = "26746"                 
+dsn_uid = "admin"        
+dsn_pwd = "SUKFLLJHQIUQQJLE"      
+
+
+
+
+try:
+    conn_string = "host="+dsn_hostname+" port="+dsn_port+" dbname="+dsn_database+" user="+dsn_uid+" password="+dsn_pwd
+    print "Connecting to database\n	->%s" % (conn_string)
+    conn=psycopg2.connect(conn_string)
+    print "Connected!\n"
+except:
+    print "Unable to connect to the database."
+
 
 
 @app.route('/')
@@ -21,3 +44,5 @@ if __name__ == '__main__':
     else:
         port, debug = 5000, True
     app.run(host='0.0.0.0', port=port, debug=debug)
+
+
